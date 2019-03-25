e2c011620a4fc185252d0eedbf9d28b47747d7b2
Alassane Ndiaye <alassane.ndiaye@gmail.com>
2019-03-25 19:16:30 -0400

    Forced DHCP server to be called with the --init flag.

--- image-build-context/entrypoint.sh
+++ image-build-context/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+if [[ $(ps -p 1 -o comm=) != "init" ]]
+then
+    echo "This container must be started with an init process."
+    echo "Please use the --init flag in your run command."
+    exit 1
+fi
 ln -fs /conf/* /etc/dhcp/
 exec dhcpd -f
